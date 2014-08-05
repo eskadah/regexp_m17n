52cd81bfa476aed15583d3944fad19523be34ef3
eskadah <eskadah@gmail.com>
2014-08-05 15:23:52 -0400

    remove test_empty_string
    Removed test for empty string because it is unnecessary in the new implementation

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -1,10 +1,5 @@
-# encoding: utf-8
-
-#
-
-
 require 'minitest/autorun'
-require_relative '../lib/regexp_m17n'
+
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
@@ -22,22 +17,5 @@ class RegexpTest < MiniTest::Unit::TestCase
         assert(shell_output.include? '\x2E')
       end
     end
-  end
-
-  def test_empty_string
-    Encoding.list.each do |enc|
-      begin
-        refute(RegexpM17N.non_empty?(''.encode(enc)),"#{enc} failed the assertion")
-      rescue Encoding::ConverterNotFoundError
-        enc_name = enc.name
-        shell_output = ''
-        IO.popen('irb', 'r+') do |pipe|
-          pipe.puts("''.force_encoding(\'" + enc_name +"\')")
-          pipe.close_write
-          shell_output = pipe.read
-        end
-        refute(shell_output.include? '\x2E')
-      end
-    end
-  end
+  end 
 end
