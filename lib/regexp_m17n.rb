9601436b90e83b9a0678a0263ff0c56acd96d866
eskadah <eskadah@gmail.com>
2014-08-05 15:29:55 -0400

    Forgot to add private methods

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -6,3 +6,16 @@ module RegexpM17N
      converted_string = irb_convert(str.encoding,str)
      Regexp.new('^.+$'.encode(converted_string.encoding),Regexp::FIXEDENCODING) =~ converted_string
   end
+
+private
+
+def self.irb_convert(dum, str)
+        shell_output = ''
+        IO.popen('irb', 'r+') do |pipe|
+         pipe.puts ( str.inspect + ".force_encoding(\'" + dum.name + "\').force_encoding(\'" + Encoding::UTF_8.name + "\')")
+          pipe.close_write
+          shell_output = pipe.read
+        end
+       shell_output.split("\n").last
+  end
+end
