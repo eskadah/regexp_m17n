7949e86696f7253397195447aa670f2425725c13
eskadah <eskadah@gmail.com>
2014-08-05 01:32:53 -0400

    update RegxpM17N
    Use Regexp#new to force encoding to be that of the input string. Also compare dummy encoded empty string to input string to ensure that the input string is not empty

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,8 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    unless str.encoding.dummy?
+      return Regexp.new('^.+$'.encode(str.encoding),Regexp::FIXEDENCODING) =~ str
+    end
+    ''.encode(str.encoding) != str
   end
 end
